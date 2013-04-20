797930aad436703595bb71804d73fd566f248934
xurxof <xurxof@gmail.com>
2013-04-20 23:28:14 +0200

    Initial Stuct

---
+++ NewsletterSaver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewsletterSaver {
+    class Program {
+        static void Main(string[] args) {
+            
+        }
+    }
+}
