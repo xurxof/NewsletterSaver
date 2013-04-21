ed63c68a653758e7e29a43bc9c8cd539bd493d10
xurxof <xurxof@gmail.com>
2013-04-21 19:22:38 +0200

    HtmlLocalSaverTest.Save_TextIsNull_DoNothingReturnFalse

---
+++ NewsletterSaver/HTMLLocalSaver.cs
@@ -0,0 +1,13 @@
+﻿using SystemWrapper.IO;
+
+namespace NewsletterSaver {
+    public sealed class HtmlLocalSaver {
+        public HtmlLocalSaver(IFileWrap fileWrap) {
+        
+        }
+
+        public bool Save(object remoteHtmlText, string filePath) {
+            return false;
+        }
+    }
+}
---
+++ NewsletterSaver/Tests/HTMLLocalSaverTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace NewsletterSaver.Tests {
+    [TestFixture]
+    internal sealed class HtmlLocalSaverTest {
+        [Test]
+        public void Save_TextIsNull_DoNothingReturnFalse() {
+            // arrange
+
+            HtmlLocalSaver Saver = new HtmlLocalSaver(null);
+            // action
+            bool Saved = Saver.Save(null, "file.txt");
+            // assert
+            Assert.IsFalse(Saved);
+        }
+    }
+}
