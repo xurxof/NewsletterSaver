4d44e95bdd08c9231cd37bdf201f8a9f701d8e0f
xurxof <xurxof@gmail.com>
2013-04-24 00:18:13 +0200

    Refactor tests

--- NewsletterSaver/Tests/InternalConfigManagerTest.cs
+++ NewsletterSaver/Tests/InternalConfigManagerTest.cs
@@ -6,33 +6,44 @@ using NUnit.Framework;
 namespace NewsletterSaver.Tests {
     [TestFixture]
     internal sealed class InternalConfigManagerTest {
-
         [Test]
         public void Read_IfFileNotExists_ReturnsNull() {
             // arrange
-            Mock<IFileWrap> File = new Mock<IFileWrap>();
-            Mock<IApplicationWrap> Application= new Mock<IApplicationWrap>();
-            Application.Setup(d=>d.ExecutablePath).Returns(@"C:\archivos de programa\test\");
-            File.Setup(s=>s.Exists(@"C:\archivos de programa\test\maxdate.txt")).Returns(false);
+
+            var Application = GetApllicationMock();
+            var File = GetFileWrapMock(false);
             InternalConfigManager Manager = new InternalConfigManager(File.Object, Application.Object);
             // action
-            DateTime? Date = Manager.GetMaxDate();
+            var Date = Manager.GetMaxDate();
             // assert
             Assert.IsNull(Date);
         }
+
+
         [Test]
         public void Read_IfFileExists_ReturnsDate() {
             // arrange
-            Mock<IFileWrap> File = new Mock<IFileWrap>();
-            Mock<IApplicationWrap> Application = new Mock<IApplicationWrap>();
-            Application.Setup(d => d.ExecutablePath).Returns(@"C:\archivos de programa\test\");
-            File.Setup(s => s.Exists(@"C:\archivos de programa\test\maxdate.txt")).Returns(true);
+            var File = GetFileWrapMock(true);
+            var Application = GetApllicationMock();
+
             File.Setup(s => s.ReadAllLines(@"C:\archivos de programa\test\maxdate.txt")).Returns(new[] {@"07/08/2013"});
             InternalConfigManager Manager = new InternalConfigManager(File.Object, Application.Object);
             // action
-            DateTime? Date = Manager.GetMaxDate();
+            var Date = Manager.GetMaxDate();
             // assert
-            Assert.AreEqual(DateTime.Parse("07/08/2013 00:00"),Date.Value);
+            Assert.AreEqual(DateTime.Parse("07/08/2013 00:00"), Date.Value);
+        }
+
+        private static Mock<IApplicationWrap> GetApllicationMock() {
+            var Application = new Mock<IApplicationWrap>();
+            Application.Setup(d => d.ExecutablePath).Returns(@"C:\archivos de programa\test\");
+            return Application;
+        }
+
+        private static Mock<IFileWrap> GetFileWrapMock(bool exist) {
+            var File = new Mock<IFileWrap>();
+            File.Setup(s => s.Exists(@"C:\archivos de programa\test\maxdate.txt")).Returns(exist);
+            return File;
         }
     }
 }
