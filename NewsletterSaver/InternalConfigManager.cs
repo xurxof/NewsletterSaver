2939d1edb6ba4cad2457faff98d91f7678c34f7b
xurxof <xurxof@gmail.com>
2013-04-24 00:15:14 +0200

    InternalConfigManager Read_IfFileExists_ReturnsDate

--- NewsletterSaver/InternalConfigManager.cs
+++ NewsletterSaver/InternalConfigManager.cs
@@ -1,12 +1,21 @@
 using System;
+using System.IO;
 using SystemWrapper.IO;
 
 namespace NewsletterSaver {
     internal sealed class InternalConfigManager {
-        public InternalConfigManager(IFileWrap fileWrap, IApplicationWrap directoryWrap) {}
+        private readonly IFileWrap _FileWrap;
+        private readonly IApplicationWrap _DirectoryWrap;
+
+        public InternalConfigManager(IFileWrap fileWrap, IApplicationWrap directoryWrap) {
+            _FileWrap = fileWrap;
+            _DirectoryWrap = directoryWrap;
+        }
 
         internal DateTime? GetMaxDate() {
-            return null;
+            var MaxDateFilePath = Path.Combine(_DirectoryWrap.ExecutablePath, "maxdate.txt");
+            if (!_FileWrap.Exists(MaxDateFilePath)) return null;
+            return DateTime.Parse(_FileWrap.ReadAllLines(MaxDateFilePath)[0]);
         }
     }
 }
--- NewsletterSaver/Tests/InternalConfigManagerTest.cs
+++ NewsletterSaver/Tests/InternalConfigManagerTest.cs
@@ -20,6 +20,19 @@ namespace NewsletterSaver.Tests {
             // assert
             Assert.IsNull(Date);
         }
-	  
+        [Test]
+        public void Read_IfFileExists_ReturnsDate() {
+            // arrange
+            Mock<IFileWrap> File = new Mock<IFileWrap>();
+            Mock<IApplicationWrap> Application = new Mock<IApplicationWrap>();
+            Application.Setup(d => d.ExecutablePath).Returns(@"C:\archivos de programa\test\");
+            File.Setup(s => s.Exists(@"C:\archivos de programa\test\maxdate.txt")).Returns(true);
+            File.Setup(s => s.ReadAllLines(@"C:\archivos de programa\test\maxdate.txt")).Returns(new[] {@"07/08/2013"});
+            InternalConfigManager Manager = new InternalConfigManager(File.Object, Application.Object);
+            // action
+            DateTime? Date = Manager.GetMaxDate();
+            // assert
+            Assert.AreEqual(DateTime.Parse("07/08/2013 00:00"),Date.Value);
+        }
     }
 }
